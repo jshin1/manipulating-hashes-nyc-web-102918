7a059bec792257ae0d7812acecd11d1f499f61ff
Ji S <j2yshin@gmail.com>
2018-10-18 15:22:21 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -16,17 +16,11 @@ def first_challenge
   #your code here
   
   contacts.each do |person, data|
-    
     data.each do |attribute, value|
-      
       if attribute == :favorite_icecream_flavors
         value.delete_if {|value| value == "strawberry"}
       end
-      
     end
-    
-    contacts
-    
   end
   
   
