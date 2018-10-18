aaf313c63173289e3826ac5d759802caf732891c
Ji S <j2yshin@gmail.com>
2018-10-18 15:18:49 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,12 @@ def first_challenge
 
   #your code here
   
-  
+  contacts.each do |person, data|
+    
+    data.each do |attribute, value|
+      
+      if attribute == :favorite_icecream_flavors
+        value.delete_if
   
   
 
